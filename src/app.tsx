2c43a95a5bd22d80a22c8beef1e17df1bcb531dd
frixaco <rustam21ashurmatov@gmail.com>
2025-12-02 11:30:03 +0500

    prepare for next iteration

--- src/app.tsx
+++ src/app.tsx
@@ -1,54 +1,51 @@
-import { useEffect, useState } from "react";
-import { useKeyboardShortcut } from "./keyboard-shortcuts";
-import { Viewport } from "./viewport";
-import { Titlebar } from "./titlebar";
-import { Drawer } from "./drawer";
-import { isTauri, getPlatformName } from "./utils";
-
 import "./global.css";
 
 function App() {
-  const [openSheet, setOpenSheet] = useState(false);
-
-  useKeyboardShortcut([
-    {
-      key: "n",
-      ctrlKey: true,
-      callback: () => {
-        console.log("opening sheet");
-        setOpenSheet((p) => !p);
-      },
-    },
-    {
-      key: "Escape",
-      callback: () => {
-        if (openSheet) {
-          setOpenSheet(false);
-        }
-      },
-    },
-  ]);
-
-  const [isWindows, setIsWindows] = useState(false);
-
-  useEffect(() => {
-    if (!isTauri) return;
-
-    (async () => {
-      setIsWindows((await getPlatformName())!.isWindows);
-    })();
-  }, []);
-
   return (
-    <main className="relative flex h-screen flex-col overflow-hidden bg-[#d7d8dd]">
-      {/* <Debug /> */}
-      {isTauri && isWindows && <Titlebar openSheet={openSheet} />}
+    <main className="flex h-screen flex-col max-w-4xl mx-auto relative">
+      <section
+        id="convo"
+        className="flex flex-col h-full bg-red-500 overflow-auto gap-2"
+      >
+        {Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]).map((n) => (
+          <div key={n} className="bg-white flex group gap-4">
+            <div className="sticky bg-yellow-500 h-fit group-hover:opacity-100 flex opacity-0 flex-col gap-1 text-xs">
+              <button>BM</button>
+              <button>CY</button>
+              <button>BR</button>
+            </div>
 
-      {/* Viewport */}
-      <Viewport />
+            <div className="border">
+              <h1>{n}</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Cupiditate maiores neque aut hic, tempora commodi sequi
+                temporibus error veritatis possimus fugit iste a, impedit
+                consequuntur odio aliquam dignissimos? Tempora modi perspiciatis
+                similique dicta quam, vel assumenda ducimus id non ut amet esse
+                nulla, ea reprehenderit repudiandae illum sequi repellat quos.
+                Officia iusto fugiat voluptas repellendus, nam dicta itaque
+                eveniet maxime possimus similique ad nisi harum repudiandae
+                doloribus explicabo inventore quos quo quidem dignissimos earum
+                quis? Voluptatum temporibus vel, recusandae ut pariatur maxime
+                obcaecati, nesciunt ipsa ea officiis ex consectetur asperiores
+                incidunt rem, cumque quo voluptates ratione dignissimos
+                voluptatem facere dolorem?
+              </p>
+            </div>
+          </div>
+        ))}
+      </section>
 
-      {/* Bottom drawer */}
-      <Drawer isOpen={openSheet} />
+      <section id="input" className="sticky bg-blue-500 flex flex-col">
+        <input type="text" placeholder="Enter prompt" />
+
+        <select>
+          <option>GPT5.1</option>
+          <option>Gemini 3 Pro</option>
+          <option>Claude Opus 4.5</option>
+        </select>
+      </section>
     </main>
   );
 }
---
+++ src/canvas/app.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { useKeyboardShortcut } from "./keyboard-shortcuts";
+import { Viewport } from "./viewport";
+import { Titlebar } from "./titlebar";
+import { Drawer } from "./drawer";
+import { isTauri, getPlatformName } from "./utils";
+
+import "./global.css";
+
+function App() {
+  const [openSheet, setOpenSheet] = useState(false);
+
+  useKeyboardShortcut([
+    {
+      key: "n",
+      ctrlKey: true,
+      callback: () => {
+        console.log("opening sheet");
+        setOpenSheet((p) => !p);
+      },
+    },
+    {
+      key: "Escape",
+      callback: () => {
+        if (openSheet) {
+          setOpenSheet(false);
+        }
+      },
+    },
+  ]);
+
+  const [isWindows, setIsWindows] = useState(false);
+
+  useEffect(() => {
+    if (!isTauri) return;
+
+    (async () => {
+      setIsWindows((await getPlatformName())!.isWindows);
+    })();
+  }, []);
+
+  return (
+    <main className="relative flex h-screen flex-col overflow-hidden bg-[#d7d8dd]">
+      {/* <Debug /> */}
+      {isTauri && isWindows && <Titlebar openSheet={openSheet} />}
+
+      {/* Viewport */}
+      <Viewport />
+
+      {/* Bottom drawer */}
+      <Drawer isOpen={openSheet} />
+    </main>
+  );
+}
+
+export default App;
